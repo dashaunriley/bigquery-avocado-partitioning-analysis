7644637d84eacd74711ff7252c99104499c33058
dashaunriley <dashaunriley@indigosound.org>
2026-05-09 13:51:06 -0400

    Remove duplicate SQL file

--- sql/sql/sql/sql/performance_query.sql
+++
@@ -1,9 +0,0 @@
-SELECT
- year,
- COUNT(*) AS number_avocados,
- SUM(TotalVolume) as sum_TotalVolume,
- SUM(AveragePrice) as sum_AveragePrice
-FROM `mydataset.avocados_clustered`
-WHERE type = 'organic'
-GROUP BY year
-ORDER BY year ASC;
